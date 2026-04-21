af91ae17ccbb2e08ac2bd4b048202a65e979168d
D. Gabriel <mendesdouglasgabriel30@gmail.com>
2026-04-21 20:37:20 -0300

    Inicio: Começando projeto de cadastro de produtos Lab.inovação III, projeto default sem nenhuma modificação

---
+++ backend/Cadastro_de_produtos/src/main/java/com/gabriel/Cad_Produtos/Cadastro_de_produtos/CadastroDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.Cad_Produtos.Cadastro_de_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeProdutosApplication.class, args);
+	}
+
+}
---
+++ backend/Cadastro_de_produtos/src/test/java/com/gabriel/Cad_Produtos/Cadastro_de_produtos/CadastroDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.Cad_Produtos.Cadastro_de_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
